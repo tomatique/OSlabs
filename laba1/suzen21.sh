f7a0bdc1cd71a6d754728f85c314b212d959d8bb
tomatique <49810729+tomatique@users.noreply.github.com>
2019-06-01 13:30:00 +0300

    Update suzen21.sh

--- laba1/suzen21.sh
+++ laba1/suzen21.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-rm [0-9]*.txt
-rm [a-z]*.png
-rm test-*.log
-ls -a
+#!bin/bash
+find . -type f | wc -l #check count files
+rm [0-9].txt [a-z].png test-*.log
+echo "Flag is: $(ls | grep -o '[0-9a-zA-Z]\{28\}')"
